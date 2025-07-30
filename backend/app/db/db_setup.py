0fa8def1c5bdbe32480b56377d0893974786418f
Joseph Newville <joenewvb@gmail.com>
2025-07-29 22:01:17 -0500

    Add database setup configuration for SQLAlchemy

---
+++ backend/app/db/db_setup.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+#SQLite for now; might swap later with postgreSQL
+SQLALCHEMY_DATABASE_URL = "sqlite://./finance.db"
+
+#SQLAlchemy egine connects python to the DB
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+#Session generator for DB access
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+#Base class for all models
+base = declarative_base()
